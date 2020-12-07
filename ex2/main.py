c848651fab87b3d68aa9a4d2a697780c873495c9
PeanutI3utter <sijunkwon@gmail.com>
2020-12-07 14:04:31 +0100

    Refactoring using flake

--- ex2/main.py
+++ ex2/main.py
@@ -2,17 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import abc
 
+
 class LQRSim(abc.ABC):
     """
     Class that simulate the LQR system
     """
     def __init__(self) -> None:
-        self.s_hist = np.array([np.random.multivariate_normal(np.zeros((2,)), np.eye(2))]).reshape(2, 1)
+        self.s_hist = np.array(
+            [np.random.multivariate_normal(np.zeros((2,)), np.eye(2))]
+        ).reshape(2, 1)
         self.a_hist = np.zeros((0, ))
         self.T = 50
         self.A_t = np.array([
             [1, .1],
-            [0 , 1]
+            [0, 1]
         ])
         self.B_t = np.array([0, .1]).reshape(-1, 1)
         self.b_t = np.array([5., 0.])
@@ -60,13 +63,16 @@ class LQRSim(abc.ABC):
 
     def calc_reward(self):
         if not self.ran:
-            raise Exception("The simulation must be run before rewards can calculated")
+            raise Exception(
+                "The simulation must be run before rewards can calculated"
+            )
         self.reward = np.zeros((self.T, ))
         err = (self.s_hist[:, -1] - self.r_t[:, -1]).reshape(-1, 1)
         self.reward[-1] = -err.T@self.R_t[-1]@err
         for t in range(self.T - 2, -1, -1):
             err = (self.s_hist[:, t] - self.r_t[:, t])
-            self.reward[t] = -err.T@self.R_t[t]@err-self.a_hist[t].T*self.H_t*self.a_hist[t]
+            self.reward[t] = -err.T@self.R_t[t]@err-self.a_hist[t].T *\
+                self.H_t*self.a_hist[t]
         return self.reward
 
 
@@ -74,7 +80,8 @@ class LQR1(LQRSim):
     def step_func(self, t):
         current_state = self.s_hist[:, t].reshape(-1, 1)
         action = -self.K_t@current_state + self.k_t
-        noise = np.random.multivariate_normal(self.b_t, self.Sigma_T).reshape(-1, 1)
+        noise = np.random.multivariate_normal(self.b_t, self.Sigma_T)\
+            .reshape(-1, 1)
         next_state = self.A_t@current_state + self.B_t*action + noise
         return action, next_state
 
@@ -86,9 +93,11 @@ class LQR_PCONTROLLER(LQRSim):
 
     def step_func(self, t):
         current_state = self.s_hist[:, t].reshape(-1, 1)
-        action = self.K_t@(self.s_des[:, t].reshape(-1, 1) - current_state) + self.k_t
-        noise = np.random.multivariate_normal(self.b_t, self.Sigma_T).reshape(-1, 1)
-        next_state = self.A_t@current_state + self.B_t*action + noise
+        action = self.K_t@(self.s_des[:, t].reshape(-1, 1) - current_state) + \
+            self.k_t
+        noise = np.random.multivariate_normal(self.b_t, self.Sigma_T)\
+            .reshape(-1, 1)
+        next_state = self.A_t@current_state + self.B_t * action + noise
         return action, next_state
 
 
@@ -100,19 +109,28 @@ class LQR_OPTIMAL(LQRSim):
         self.v_t = np.zeros_like(self.r_t)
         self.v_t[:, -1] = self.R_t[-1]@self.r_t[:, -1]
         for t in range(self.T - 2, -1, -1):
-            M_t = (self.B_t/(self.H_t + \
-                self.B_t.T@self.V_t[t+1]@self.B_t))@self.B_t.T@self.V_t[t + 1]@self.A_t
-            self.V_t[t] = self.R_t[t] + (self.A_t - M_t).T@self.V_t[t + 1]@self.A_t
-            self.v_t[:, t] = (self.R_t[t]@self.r_t[:, t].reshape(-1, 1) + \
-                (self.A_t - M_t).T@(self.v_t[:, t + 1].reshape(-1, 1) - self.V_t[t + 1]@self.b_t.reshape(-1, 1))).reshape(-1)
+            M_t = (self.B_t/(self.H_t +
+                             self.B_t.T@self.V_t[t+1]@self.B_t))@self.B_t.T\
+                             @ self.V_t[t + 1]@self.A_t
+            self.V_t[t] = self.R_t[t] + (self.A_t - M_t).T@self.V_t[t + 1]\
+                @ self.A_t
+            self.v_t[:, t] = (self.R_t[t]@self.r_t[:, t].reshape(-1, 1) +
+                              (self.A_t - M_t).T@(self.v_t[:, t + 1]
+                              .reshape(-1, 1) - self.V_t[t + 1]
+                              @ self.b_t.reshape(-1, 1))).reshape(-1)
 
     def step_func(self, t):
         current_state = self.s_hist[:, t].reshape(-1, 1)
-        action = -(self.H_t+self.B_t.T@self.V_t[t]@self.B_t)**-1*self.B_t.T@(self.V_t[t]@(self.A_t@current_state + self.b_t.reshape(-1, 1)) - self.v_t[:, t].reshape(-1, 1))
-        noise = np.random.multivariate_normal(self.b_t, self.Sigma_T).reshape(-1, 1)
+        action = -(self.H_t+self.B_t.T@self.V_t[t]@self.B_t)**-1\
+            * self.B_t.T@(self.V_t[t] @
+                          (self.A_t@current_state + self.b_t.reshape(-1, 1))
+                          - self.v_t[:, t].reshape(-1, 1))
+        noise = np.random.multivariate_normal(self.b_t, self.Sigma_T)\
+            .reshape(-1, 1)
         next_state = self.A_t@current_state + self.B_t*action + noise
         return action, next_state
-    
+
+
 class Plotter:
     i = 0
 
@@ -138,7 +156,6 @@ class Plotter:
         time_r_a = np.linspace(0, 49, 50)
         [plt.figure(ii) for ii in range(self.i)]
 
-
         states = np.array([sim.s_hist for sim in sims])
         states_mean = np.mean(states, axis=0)
         states_var = np.var(states, axis=0)
@@ -147,7 +164,9 @@ class Plotter:
         action_mean = np.mean(actions, axis=0)
         action_var = np.var(actions, axis=0)
 
-        cumulative_rewards = np.array([np.cumsum(sim.calc_reward()) for sim in sims])
+        cumulative_rewards = np.array(
+            [np.cumsum(sim.calc_reward()) for sim in sims]
+        )
         reward_mean = np.mean(cumulative_rewards, axis=0)
         reward_std = np.sqrt(np.var(cumulative_rewards, axis=0))
         print(f'{prefix} Mean cumulative reward {reward_mean[-1]}')
@@ -157,15 +176,29 @@ class Plotter:
             plt.figure(to_plot['s1'])
             plt.title(f'[{prefix}] Mean of First dimension of state over time')
             plt.plot(time_states, states_mean[0])
-            plt.fill_between(time_states, states_mean[0] - 2 * np.sqrt(states_var[0]), states_mean[0] + 2 * np.sqrt(states_var[0]), color='orange', alpha=.4)
+            plt.fill_between(
+                time_states,
+                states_mean[0] - 2 * np.sqrt(states_var[0]),
+                states_mean[0] + 2 * np.sqrt(states_var[0]),
+                color='orange',
+                alpha=.4
+            )
             plt.xlabel('Time t')
             plt.ylabel('First dimension of the state')
 
         if s2:
             plt.figure(to_plot['s2'])
-            plt.title(f'[{prefix}] Mean of Second dimension of state over time')
+            plt.title(
+                f'[{prefix}] Mean of Second dimension of state over time'
+            )
             plt.plot(time_states, states_mean[1])
-            plt.fill_between(time_states, states_mean[1] - 2 * np.sqrt(states_var[1]), states_mean[1] + 2 * np.sqrt(states_var[1]), color='orange', alpha=.4)
+            plt.fill_between(
+                time_states,
+                states_mean[1] - 2 * np.sqrt(states_var[1]),
+                states_mean[1] + 2 * np.sqrt(states_var[1]),
+                color='orange',
+                alpha=.4
+            )
             plt.xlabel('Time t')
             plt.ylabel('Second dimension of the state')
 
@@ -173,7 +206,13 @@ class Plotter:
             plt.figure(to_plot['a'])
             plt.title(f'[{prefix}] Mean Action over time')
             plt.plot(time_r_a, action_mean)
-            plt.fill_between(time_r_a, action_mean - 2 * np.sqrt(action_var), action_mean + 2 * np.sqrt(action_var), color='orange', alpha=.4)
+            plt.fill_between(
+                time_r_a,
+                action_mean - 2 * np.sqrt(action_var),
+                action_mean + 2 * np.sqrt(action_var),
+                color='orange',
+                alpha=.4
+            )
             plt.xlabel('Time t')
             plt.ylabel('Action')
 
@@ -194,7 +233,7 @@ if __name__ == "__main__":
         sim.run()
     plotter.plot(lqr, prefix='Zero control')
     """
-    
+
     # b)
     zeros = np.zeros((2, 50))
 
