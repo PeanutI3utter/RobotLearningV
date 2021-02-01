7c385c730e8be5738b348328951487fa505bdb56
PeanutI3utter <sijunkwon@gmail.com>
2021-02-01 17:07:50 +0100

    removed unused variable

--- ex4/getProMPBasis.py
+++ ex4/getProMPBasis.py
@@ -1,16 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def getProMPBasis(dt, nSteps, n_of_basis, bandwidth):
 
-    time = np.arange(dt,nSteps*dt,dt)
-
-    variance = np.sqrt(bandwidth)
+    time = np.arange(dt, nSteps * dt, dt)
     basis = np.linspace(-2 * bandwidth, dt * nSteps + 2 * bandwidth, n_of_basis)
     Phi = np.zeros((nSteps, n_of_basis))
     for i, t in enumerate(time):
         for j in range(n_of_basis):
-            Phi[i, j] = np.exp(-.5*(t - basis[j]) ** 2 / bandwidth**2)
+            Phi[i, j] = np.exp(-0.5 * (t - basis[j]) ** 2 / bandwidth ** 2)
         Phi[i] = Phi[i] / np.sum(Phi[i])
 
     return Phi
